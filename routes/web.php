96763926981aee5664372fc9a1afb70bb82d6131
Krystian <krystian.wojcik95@gmail.com>
2020-12-13 13:34:58 +0100

    Add route clear cache

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "Cache is cleared";
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

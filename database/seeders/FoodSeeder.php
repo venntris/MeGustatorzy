cae938e34e4dfb01fd6db21ab5e2afe5da315736
Krystian <krystian.wojcik95@gmail.com>
2021-01-07 18:04:52 +0100

    Add seaders Food and Room

---
+++ database/seeders/FoodSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class FoodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('foods')->insert([
+            'name' => 'piwo',
+        ]);
+        DB::table('foods')->insert([
+            'name' => 'pizza',
+        ]);
+        DB::table('foods')->insert([
+            'name' => 'golonka',
+        ]);
+        DB::table('foods')->insert([
+            'name' => 'chabanina',
+        ]);
+    }
+}

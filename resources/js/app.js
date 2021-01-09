5c4a0ddd53281d33d4eb0f58a76ca27e0161704a
mtrefler <mtrefler@1106TG2>
2021-01-09 20:46:14 +0100

    navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,6 +23,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+Vue.component('navbar-component', require('./components/NavbarComponent.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application

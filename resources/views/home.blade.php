bd2b2fac32707e31f2f3eb92ecee33be72c03622
Mike-Soft <49808920+Mike-Soft@users.noreply.github.com>
2021-01-09 15:00:04 +0100

    komit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,10 +11,12 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
+                            {{ __('You are logged in!') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    @if(auth())
+                    
+                    @endif
                 </div>
             </div>
         </div>
